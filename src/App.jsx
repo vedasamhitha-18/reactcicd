0842ccc929d178f85bf6102c849475240dc82aac
vedasamhitha-18 <vedasamhitha9900@gmail.com>
2025-08-06 09:48:50 +0530

    my 2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1> Hii this is git action experiment</h1>
-      <h2>Version 1 of my project</h2>
+      <h2>Version 2 of my project</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
